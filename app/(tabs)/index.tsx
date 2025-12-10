62507f65518482ef2d045fdf9b43449a24a253f3
Kahraman <80291704+kahramanX@users.noreply.github.com>
2025-12-10 21:42:35 +0300

    added search bar components

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,24 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import SearchBar from "@/components/SearchBar";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { Image, ScrollView, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-6xl font-bold italic text-blue-500 text-justify">
-        HELLOOOOO WORLD
-      </Text>
-      <Text>This is a normal text.</Text>
-      <Link href="/" className="text-blue-500 text-xl p-5 bg-red-100">
-        Go to Onboarding
-      </Link>
-      <Link
-        href="/movie/avenger-porn"
-        className="text-blue-500 text-xl p-5 bg-red-100"
+    <View className="flex-1 bg-primary">
+      <Image
+        source={images.bg}
+        className="absolute w-full z-0"
+        resizeMode="cover"
+      />
+      <ScrollView
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
       >
-        Go to Movie Details
-      </Link>
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+        <SearchBar placeholder="Search..." />
+      </ScrollView>
     </View>
   );
 }
