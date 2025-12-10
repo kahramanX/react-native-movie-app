25741631cea703f8bec5cd4c7781738a7d8346bb
Kahraman <80291704+kahramanX@users.noreply.github.com>
2025-12-10 22:56:02 +0300

    useFetch used in index page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,23 @@
 import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
-import { Image, ScrollView, View } from "react-native";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
+import { useRouter } from "expo-router";
+import { ActivityIndicator, Image, ScrollView, Text, View } from "react-native";
 
 export default function Index() {
+  const router = useRouter();
+  const {
+    data: moviesData,
+    loading: moviesLoading,
+    error: moviesError,
+    refetch: refetchMovies,
+    reset: resetMovies,
+  } = useFetch(() => fetchMovies({ query: "avatar" }), true);
+
   return (
-    <View className="flex-1 bg-primary h-full border-2 border-red-500">
+    <View className="flex-1 bg-primary h-full">
       <Image
         source={images.bg}
         className="absolute w-full z-0"
@@ -20,7 +32,26 @@ export default function Index() {
         }}
       >
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
-        <SearchBar placeholder="Search.." />
+        <View>
+          {moviesLoading ? (
+            <ActivityIndicator
+              size="large"
+              color="#0000ff"
+              className="mt-10 self-center"
+            />
+          ) : moviesError ? (
+            <Text className="text-white text-xl mt-10">
+              Error : {JSON.stringify(moviesError, null, 2)}
+            </Text>
+          ) : (
+            <>
+              <SearchBar placeholder="Search.." />
+              <Text className="text-white text-lg font-bold mt-5 mb-3">
+                Latest Movies
+              </Text>
+            </>
+          )}
+        </View>
       </ScrollView>
     </View>
   );
